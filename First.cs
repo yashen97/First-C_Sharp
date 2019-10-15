f2ad918f79bb99f0b3d223229ab7dfc576bbeda7
DevFern <45023645+Suchithfernando@users.noreply.github.com>
2019-10-15 10:07:43 +0530

    update description
    update description to learn a new beginner to understand the task of this code

--- First.cs
+++ First.cs
@@ -44,6 +44,24 @@ namespace DesktopApp1
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
+        //
+
+    The first line of the program using System; - the using keyword is used to include the System namespace in the program. A program generally has multiple using statements.
+
+    The next line has the namespace declaration. A namespace is a collection of classes. The HelloWorldApplication namespace contains the class HelloWorld.
+
+    The next line has a class declaration, the class HelloWorld contains the data and method definitions that your program uses. Classes generally contain multiple methods. Methods define the behavior of the class. However, the HelloWorld class has only one method Main.
+
+    The next line defines the Main method, which is the entry point for all C# programs. The Main method states what the class does when executed.
+
+    The next line /*...*/ is ignored by the compiler and it is put to add comments in the program.
+
+    The Main method specifies its behavior with the statement Console.WriteLine("Hello World");
+
+    WriteLine is a method of the Console class defined in the System namespace. This statement causes the message "Hello, World!" to be displayed on the screen.
+
+    The last line Console.ReadKey(); is for the VS.NET Users. This makes the program wait for a key press and it prevents the screen from running and closing quickly when the program is launched from Visual Studio .NET.
+
 
         }
     }
